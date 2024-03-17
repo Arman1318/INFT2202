0339472b92b880d424dc8fdd81596e0b2706cae5
Arman1318 <arman.dodhiya@dcmail.ca>
2024-03-17 00:34:07 -0400

    fetch Api

--- src/js/blog.js
+++ src/js/blog.js
@@ -1,1 +1,16 @@
 document.addEventListener('DOMContentLoaded', initializeApp);
+
+
+function fetchData(container) {
+    fetch('https://jsonplaceholder.typicode.com/posts')
+        .then(response => response.json())
+        .then(posts => {
+            posts.forEach(post => {
+                const postCard = makeCard(post);
+                container.appendChild(postCard);
+            });
+        })
+        .catch(error => {
+            console.error('Error fetching posts:', error);
+        });
+}
